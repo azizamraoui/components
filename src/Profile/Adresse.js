82ec1419f8acb98ed9115973124facd85d0da2f0
azizamraoui <amraouiaziz4@gmail.com>
2021-03-19 22:08:43 +0100

    components

---
+++ src/Profile/Adresse.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../App.js';
+
+function Adresse() {
+    return (
+        <div>
+             <h1 className="Adress">amraouiaziz4@gmail.com</h1>
+        </div>
+    )
+}
+
+export default Adresse
